b42a1834c6b1f1c7e77f661f1806e99910426666
chris-bes <chris@beyondessential.com.au>
2023-11-21 14:49:30 +1100

    test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,15 @@
 import React from "react";
-import styled from "styled-components";
-import { TamanuLogo } from "../assets/TamanuLogo";
+// import styled from "styled-components";
+// import { TamanuLogo } from "../assets/TamanuLogo";
 
-const StyledTamanuLogo = styled(TamanuLogo)`
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  height: 100px;
-  width: auto;
-`;
+// const StyledTamanuLogo = styled(TamanuLogo)`
+//   position: absolute;
+//   top: 0px;
+//   left: 0px;
+//   height: 100px;
+//   width: auto;
+// `;
 
 export const NavBar = () => {
-  return <StyledTamanuLogo />;
+  return <></>;
 };
