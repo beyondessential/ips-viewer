25b7951c83ad85fd4aab8f5561fe08102d69bb20
chris-bes <chris@beyondessential.com.au>
2023-11-21 14:40:08 +1100

    fixed lint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { IPSForm } from "./components/IPSForm";
-import { IPSResult } from "./components/IPSResult";
+// import { IPSResult } from "./components/IPSResult";
 import { NavBar } from "./components/NavBar";
 
 export const App = () => {
